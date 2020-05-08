97cd2741417d0fca3101c3d12218aae13814df38
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-08 07:46:15 -0700

    Generated Passenger model and seeded the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_143622) do
+ActiveRecord::Schema.define(version: 2020_05_08_144310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_05_08_143622) do
     t.string "name"
     t.string "vin"
     t.boolean "available", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_num"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
