1b50311a07d306f000885be5628d4eb3d22d4b2a
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-09 06:42:46 -0700

    Added PassengersController#index and view

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,74 @@
 class PassengersController < ApplicationController
+  def index
+    @passengers = Passenger.all
+  end
+
+  # def show
+  #   driver_id = params[:id]
+  #   @driver = Driver.find_by(id: driver_id)
+  #   if @driver.nil?
+  #     head :not_found
+  #     return
+  #   end
+
+  #   @trips = @driver.trips
+  # end
+
+  # def new
+  #   @driver = Driver.new
+  # end
+
+  # def create
+  #   @driver = Driver.new(driver_params) 
+  #   if @driver.save 
+  #     redirect_to drivers_path 
+  #     return
+  #   else 
+  #     render :new 
+  #     return
+  #   end
+  # end
+
+  # def edit
+  #   @driver = Driver.find_by(id: params[:id])
+
+  #   if @driver.nil?
+  #     head :not_found # ?
+  #     return
+  #   end
+  # end
+
+  # def update
+  #   @driver = Driver.find_by(id: params[:id])
+  #   if @driver.nil?
+  #     head :not_found # ?
+  #     return
+  #   elsif @driver.update(driver_params)
+  #     redirect_to drivers_path
+  #     return
+  #   else 
+  #     render :edit
+  #     return
+  #   end
+  # end
+
+  # def destroy
+  #   @driver = Driver.find_by(id: params[:id])
+
+  #   if @driver.nil?
+  #     head :not_found # ?
+  #     return
+  #   end
+
+  #   @driver.destroy
+
+  #   redirect_to drivers_path
+  #   return
+  # end
+
+  # private
+
+  # def driver_params
+  #   return params.require(:driver).permit(:name, :vin, :available)
+  # end
 end
