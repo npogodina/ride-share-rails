6ed44b3e691a09f80c32e1607342210148223746
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-08 20:53:09 -0700

    Added drivers#new & #create + view

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -11,4 +11,24 @@ class DriversController < ApplicationController
       return
     end
   end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params) 
+    if @driver.save 
+      redirect_to drivers_path 
+      return
+    else 
+      render :new 
+      return
+    end
+  end
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :available)
+  end
+
 end
