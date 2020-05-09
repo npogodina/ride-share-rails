253003f35932e9b63d13f819cee8a84eb48f4685
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-08 22:12:37 -0700

    Added Driver#average_rating and updated drivers index and show views

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -6,4 +6,18 @@ class Driver < ApplicationRecord
       (trip.cost - 1.65) * 0.8
     end
   end
+
+  def average_rating
+    average_rating = 0
+
+    rated_trips = self.trips.select { |trip| trip.rating != nil }
+    rated_trips_num = rated_trips.count
+
+    if rated_trips_num > 0
+      rating_sum = rated_trips.sum { |trip| trip.rating }
+      average_rating = rating_sum / rated_trips_num
+    end
+
+    return average_rating
+  end
 end
