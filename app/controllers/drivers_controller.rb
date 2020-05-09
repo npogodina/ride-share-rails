f796ab7689d44553b7101ce236bdb16d4736a40b
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-08 21:02:11 -0700

    Added driver#edit, #update, #destroy and edit view

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -27,6 +27,45 @@ class DriversController < ApplicationController
     end
   end
 
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found # ?
+      return
+    end
+  end
+
+  def update
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found # ?
+      return
+    elsif @driver.update(driver_params)
+      redirect_to drivers_path
+      return
+    else 
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found # ?
+      return
+    end
+
+    @driver.destroy
+
+    redirect_to drivers_path
+    return
+  end
+
+  private
+
   def driver_params
     return params.require(:driver).permit(:name, :vin, :available)
   end
