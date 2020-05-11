0f5e40bdce42838b1ae4cbad777d69da6d5efef2
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-11 07:26:28 -0700

    Added Passenger#total_trips and #total_spent and updated show view

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,14 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_spent
+    total_spent = self.trips.sum do |trip|
+      trip.cost
+    end
+  end
+
+  def total_trips
+    self.trips.count
+  end
+
 end
