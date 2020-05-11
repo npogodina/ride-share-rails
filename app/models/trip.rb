5830d07db90d5ec5391b775a4c21a67bb0befdb3
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-11 15:24:31 -0700

    Added validation for trips and updated trips show and edit views

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,8 @@
 class Trip < ApplicationRecord
   belongs_to :driver, optional: true
   belongs_to :passenger, optional: true
+
+  validates :driver_id, presence: true
+  validates :passenger_id, presence: true
+  validates :date, presence: true
 end
